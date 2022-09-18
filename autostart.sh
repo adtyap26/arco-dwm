90f807a804fb41d4c2ec7f457803ec6745d3447a
Aditya Permana <permanaaditya2606@gmail.com>
2022-09-18 16:57:02 +0700

    Modifying the configs

--- autostart.sh
+++ autostart.sh
@@ -20,14 +20,14 @@ run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
 run "nm-applet"
 run "pamac-tray"
 #run "variety"
-run "xfce4-power-manager"
+#run "xfce4-power-manager"
 #run "blueberry-tray"
 run "/usr/lib/xfce4/notifyd/xfce4-notifyd"
 run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
 picom -b  --config ~/.config/arco-dwm/picom.conf &
 run "numlockx on"
 run "volumeicon"
-run "slstatus" &
+run "dwmblocks" &
 sxhkd -c ~/.config/arco-dwm/sxhkd/sxhkdrc &
 #run "nitrogen --restore"
 #run "conky -c $HOME/.config/arco-dwm/system-overview"
